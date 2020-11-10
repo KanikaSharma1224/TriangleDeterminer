6704e4ac0b2f40e617a57fdbbe500771592cea59
Kanika Sharma <kanikasharma1224@gmail.com>
2020-11-10 21:33:00 +0530

    Triangle Determiner Spring Rest Project

---
+++ src/main/java/com/triangledeterminer/TriangleDeterminerApplication.java
@@ -0,0 +1,13 @@
+package com.triangledeterminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriangleDeterminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriangleDeterminerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triangledeterminer/TriangleDeterminerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triangledeterminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriangleDeterminerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
