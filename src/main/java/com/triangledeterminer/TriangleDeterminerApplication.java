45405657ea71f5396456ab9e4e2a7357c22954dc
Kanika Sharma <kanikasharma1224@gmail.com>
2020-11-10 22:10:46 +0530

    Initial changes to triangle app

--- src/main/java/com/triangledeterminer/TriangleDeterminerApplication.java
+++ src/main/java/com/triangledeterminer/TriangleDeterminerApplication.java
@@ -2,9 +2,17 @@ package com.triangledeterminer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TriangleDeterminerApplication {
+
+	@RequestMapping("/")
+	String display() {
+		return "Hello World from Triangle Determiner !!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TriangleDeterminerApplication.class, args);
